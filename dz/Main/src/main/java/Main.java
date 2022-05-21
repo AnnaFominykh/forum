7ea0c1a7ba4201fbddde4ef260ae92a35d771e92
AnnaFominykh <103980771+AnnaFominykh@users.noreply.github.com>
2022-05-21 06:51:35 +0500

    Add files via upload

--- dz/Main/src/main/java/Main.java
+++ dz/Main/src/main/java/Main.java
@@ -1,17 +1,30 @@
 import java.util.ArrayList;
 
-public class Main{
-    public void main (String[] args){
-        ArrayList<ua> apl =new ArrayList<>();
+public class Main {
+    public void main(String[] args) {
+        ArrayList<ua> apl = new ArrayList<>();
         user u = new user();
-        application a = new application ();
+        application a = new application();
 
         u.id("Фриск");
         u.id("Чара");
         u.id("Крис");
-        for (int s = 0; s<u.apl.size();s++){
-System.out.println();
+a.place("","");
+        a.place("","");
+        a.place("","");
+        for (int s = 0; s < u.apl.size(); s++) {
+            u.apl.get(s).GetID();
+        }
+        for (int s = 0; s < u.apl.size(); s++) {
+            System.out.println(u.apl.get(s).GetID());
+        }
 
+        for (int p = 0; p < a.apl.size(); p++) {
+            System.out.println(a.apl.get(p).GetID());
+        }
+
+        for (int i = 0; i < a.apl.size(); i++) {
+            System.out.println(a.apl.get(i).GetPlace());
+        }
     }
-
-}}
+}
--- dz/src/main/java/main.java
+++ dz/src/main/java/main.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class main{
+public class ain{
     public void main (String[] args){
         ArrayList<ua> apl =new ArrayList<>();
         User u = new User;
